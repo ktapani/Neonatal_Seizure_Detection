8167b1453289f5a70240d00014fb997b481db6a4
ktapani <karoliina.tapani@gmail.com>
2018-06-08 13:41:27 +0300

    Update get_raw_dec.m

--- Neonatal_Seizure_Detection_Algorithm/reproducibility/get_raw_dec.m
+++ Neonatal_Seizure_Detection_Algorithm/reproducibility/get_raw_dec.m
@@ -1,7 +1,7 @@
-function dec=get_raw_dec(norm_val,feats,SVMs,p)
+function dec_raw=get_raw_dec(norm_val,feats,SVMs,p)
 % labels from annotation (consensus)
 ff_ind=[1:length(feats{p}{1})]';
-dec=cell(length(feats{p}),1);
+dec_raw=cell(length(feats{p}),1);
 
 for i=1:length(feats{p})   
     ff=[];
@@ -13,7 +13,7 @@ for i=1:length(feats{p})
     mu=norm_val{p}(1:ss);
     sig=norm_val{p}((ss)+1:end);
     val=bsxfun(@rdivide,bsxfun(@minus,ff,mu),sig);
-    [ ~,dec{i}] = predict(SVMs{p},val);
-    dec{i}=dec{i}(:,2);
+    [ ~,dec_raw{i}] = predict(SVMs{p},val);
+    dec_raw{i}=dec_raw{i}(:,2);
 end
 
